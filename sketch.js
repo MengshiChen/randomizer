0ede7ac540b27f7cd029771e7c14c4e72d8d9251
MengshiChen <MengshiChen@users.noreply.github.com>
2020-03-05 21:41:49 -0800

    edited

--- sketch.js
+++ sketch.js
@@ -1,34 +1,57 @@
-let dogs = [{name:"donut", color:"yellow"}, {name:"fishball", color:"pink"}, {name:"cucumber", color:"orange"}, {name:"shiba", color:"merigold"}, {name:"bobo", color:"black"}, {name:"ben", color:"brown"}];
+let dogs = [];
+
+
 let randomIndex;
 let animating = false;
 // let counter = 0;
 let images = [];
 let imagecounter = 0;
+let startRandomizerButton;
+let addMoreButton;
+let cnv;
+let nameInputs = [];
+let firstTime = true;
 
 function preload() {
-  for (let i = 1; i <= 12; i++) {
-    images[i-1] = loadImage(`/images/glitch_${i}.jpg`);
+  //i=0, if the image count from 1, i=1, images[i-1].
+  for (let i = 0; i <= 11; i++) {
+    // images = pictures that I put
+    images[i] = loadImage(`/images/glitch_${i}.jpg`);
   }
-
 }
 
 function setup(){
-createCanvas(600, 600);
-background(200);
-text("click to randomize", 50, 50);
+cnv = createCanvas(600, 600);
+cnv.parent("#canvasDiv");
+
+background(224,194,255);
 imageMode(CENTER);
 frameRate(8);
 console.log(images);
+textSize(36);
 textStyle(BOLD);
-textAlign(CENTER);
-button = createButton ("Click to randomize");
-button.mousePressed(buttonPressed);
-button.class("randomizerButton");
+textAlign(CENTER, BOTTOM);
+fill("blue");
+// button = createButton ("Click to randomize");
+startRandomizerButton = select('#randButton')
+startRandomizerButton.mousePressed(buttonPressed);
+
+addMoreButton = select('#addMoreButton')
+addMoreButton.mousePressed(addAnotherInput);
+
+
+for(let i = 0; i < 3; i++){
+nameInputs.push(createInput());
+nameInputs[nameInputs.length - 1].parent("#inputFields");
+}
+
 // setInterval(changeBackground, 1000);
 // setTimeout(changeBackground, 1000);
 }
 
 function draw(){
+
+
   if(animating == true)
   {   clear();
     image(images[imagecounter], width / 2, height / 2);
@@ -40,23 +63,45 @@ function draw(){
 
   }
 }
+
+function addAnotherInput (){
+
+  nameInputs.push(createInput());
+  nameInputs[nameInputs.length - 1].parent("#inputFields");
+
+}
+
 function randomizer(){
   animating = false;
   if (dogs[0]){
-    background(random(200,255));
+     //background(random(194,224));
+     clear();
     randomIndex = int(random(dogs.length));
-      text(`${dogs[randomIndex].name} 's favorite color is ${dogs[randomIndex].color}`, width / 2 , height / 2 );
+      image(random(images), width / 2, height / 2)
+      text(dogs[randomIndex], width / 2 , height / 2 );
 
     // text(dogs[randomIndex].name + "'s favorite color is " + dogs[randomIndex].color, 50, 50);
+
     dogs.splice(randomIndex, 1);
   }
     else {
-      background(random(200, 255));
-    text("nothing left!", 50, 50);
+    background(224,194,255);
+    text("nothing left!", width / 2, height / 2);
     }
 
 }
 function buttonPressed(){
+
+if (firstTime == true){
+
+  for(let i = 0; i < nameInputs.length; i++){
+    dogs.push(nameInputs[i].value());}
+
+    firstTime = false;
+
+  }
+
+
   animating = true;
   setTimeout(randomizer, 2000);
 }
